1c85a1ca4657660efb5496ed2667071094a644fc
Michael <michael.ranits@gmx.at>
2020-04-26 21:36:10 +0200

    create Customer.java

---
+++ src/ranits_sales/Customer.java
@@ -0,0 +1,27 @@
+package ranits_sales;
+
+public class Customer {
+	private int customerID;
+	private String firstname;
+	
+	public Customer()
+	{
+		
+	}
+
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+}
